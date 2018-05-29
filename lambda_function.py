22865d87921ea3027d2541c941bc04f5af5224c9
Andrew Cargill <20098785+cargiggles@users.noreply.github.com>
2018-05-28 19:23:33 -0700

    Moving Functions Outside Lambda Handler
    Move functions outside Lambda handler and created new wunderground_hourly function

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
-# Fair Weather Rider Lambda v1.2
+# Fair Weather Rider Lambda v1.3
 # Andrew Cargill
-# May 11, 2018
+# May 28, 2018
 
 import boto3
 import datetime
@@ -17,50 +17,49 @@ CHANCE_RAIN = int(os.environ['CHANCE_RAIN'])
 
 BIKE_TO_WORK = True
 
-def lambda_handler(event, context):
+class HourObject(object):
+	""" An Hour Of The Day """
+	def __init__(self, hour, pop, temp):
+		self.hour = int(hour)
+		self.pop = float(pop) # chance of precipitation
+		self.temp = float(temp)
+
+	def __str__(self):
+		rep = str(self.hour) + ":00\n" + str(self.pop) + "% chance rain\n" + str(self.temp) + " F\n"
+		return rep
+
+	def bike_logic(self):
+		global BIKE_TO_WORK
+		if self.pop > CHANCE_RAIN or self.temp < LOWEST_ACCEPTABLE_TEMP:
+			BIKE_TO_WORK = False
+
+def send_sns_sms():
+    sns = boto3.client('sns')
+    print sns.publish(PhoneNumber = PHONE_NUMBER, Message = "Ride Your Bike!" + " " + "(" + time.strftime("%a") + ")")
+    # print both executes and records status codes to CloudWatch
+
+def wunderground_hourly():
     url = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/" + ZIP + ".json"
     response = urllib2.urlopen(url) # Calling Wunderground
-    weather = json.load(response) # .json is read and put into the "weather" dictionary object
+    return json.load(response) # .json is read and converted to dictionary
 
+def lambda_handler(event, context):
+    weather = wunderground_hourly()
     today = datetime.datetime.now().day # int representing today's day of the month
     commuteTimes = [8, 9, 17, 18, 19] # hours during which I would conceivibly be commuting
     hours = [] # Empty list into which I'll place (up to) five "hour" objects
-
-    class HourObject(object):
-    	""" An Hour Of The Day """
-    	def __init__(self, hour, pop, temp):
-    		self.hour = int(hour)
-    		self.pop = int(pop) # chance of precipitation
-    		self.temp = int(temp)
-
-    	def __str__(self):
-    		rep = str(self.hour) + ":00\n" + str(self.pop) + "% chance rain\n" + str(self.temp) + " F\n"
-    		return rep
-
-    	def bike_logic(self):
-    		global BIKE_TO_WORK
-    		if self.pop > CHANCE_RAIN or self.temp < LOWEST_ACCEPTABLE_TEMP:
-    			BIKE_TO_WORK = False
-
-    def send_sns_sms():
-        sns = boto3.client('sns')
-        print sns.publish(PhoneNumber = PHONE_NUMBER, Message = "Ride Your Bike!" + " " + "(" + time.strftime("%a") + ")")
-        # print both executes and records status codes to CloudWatch
-
-    # MAIN
 
     for hour in weather['hourly_forecast']: # API returns 36 hour forecasts
     	if int(hour['FCTTIME']['hour']) in commuteTimes and int(hour['FCTTIME']['mday']) == today:
     		entry = HourObject(hour['FCTTIME']['hour'], hour['pop'], hour['temp']['english']) # Making object for each hour
     		hours.append(entry) # Adding hour object to list
 
-    for each in hours:
-    	each.bike_logic() # Invoke each hour object's function to test whether global BIKE_TO_WORK should be toggled off
-    	print each
+    for hour in hours:
+    	hour.bike_logic() # Invoke each hour object's function to test whether global BIKE_TO_WORK should be toggled off
+    	print hour
 
     if BIKE_TO_WORK == True:
     	send_sns_sms()
     	print time.strftime("%a %b %d") + "\nRide your bike!\nSMS Sent!"
     else:
     	print "It's too cold and/or rainy, bus it."
- 
