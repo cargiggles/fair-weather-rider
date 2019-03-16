ccae4219f194856fa5192c2feacafc374ad25c7e
Andrew Cargill <ac@samuraifight.com>
2019-03-15 17:10:15 -0700

    Version 3.0 - Dark Sky API
    Weather Underground's API has been shut down. Code has been refactored to use the Dark Sky API instead.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Summary
 
-Fair Weather Rider is a Python based script which calls the [Weather Underground API](https://www.wunderground.com/weather/api/) to determine whether or not weather conditions are favorable enough for you to comfortably commute by bike, without being rained upon. If so, it will send a text message reminding you. This particular implementation leverages the power of Amazon Web Services (AWS), specifically, Lambda and SNS.
+
+
+Fair Weather Rider is a Python based script which calls the [Dark Sky API](https://darksky.net/dev) to determine whether or not weather conditions are favorable enough for you to comfortably commute by bike, without being rained upon. If so, it will send a text message reminding you. This particular implementation leverages the power of Amazon Web Services (AWS), specifically, Lambda and SNS.
--- lambda_function.py
+++ lambda_function.py
@@ -1,65 +1,105 @@
-# Fair Weather Rider Lambda v1.3
+# Fair Weather Rider Lambda v3.0
 # Andrew Cargill
-# May 28, 2018
+# 2019-03-15 - Re-wrote To Use Dark Sky API And pytz
 
+from botocore.vendored import requests
+from darksky import forecast
+from datetime import datetime, timedelta
+from pytz import timezone
 import boto3
-import datetime
 import json
 import os
-import time
-import urllib2
+import pytz
 
-API_KEY = os.environ['API_KEY']
-PHONE_NUMBER = os.environ['PHONE_NUMBER']
-ZIP = os.environ['ZIP']
-LOWEST_ACCEPTABLE_TEMP = int(os.environ['LOWEST_ACCEPTABLE_TEMP'])
-CHANCE_RAIN = int(os.environ['CHANCE_RAIN'])
+# API Keys
+dark_sky_api_key = os.environ['dark_sky_api_key']
 
-BIKE_TO_WORK = True
+# Constants
+lat = float(os.environ['lat'])
+lng = float(os.environ['lng'])
 
-class HourObject(object):
-	""" An Hour Of The Day """
-	def __init__(self, hour, pop, temp):
-		self.hour = int(hour)
-		self.pop = float(pop) # chance of precipitation
-		self.temp = float(temp)
+# User Preferences
+lowest_acceptable_precip = int(os.environ['lowest_acceptable_precip'])
+lowest_acceptable_temp = int(os.environ['lowest_acceptable_temp'])
+phone_number = os.environ['phone_number']
 
-	def __str__(self):
-		rep = str(self.hour) + ":00\n" + str(self.pop) + "% chance rain\n" + str(self.temp) + " F\n"
-		return rep
+# Local Time Zone To Be Populated By Dark Sky API Call
+local_time_zone = "" # America/Los_Angeles
 
-	def bike_logic(self):
-		global BIKE_TO_WORK
-		if self.pop > CHANCE_RAIN or self.temp < LOWEST_ACCEPTABLE_TEMP:
-			BIKE_TO_WORK = False
+bike_to_work = True
 
-def send_sns_sms():
+class BikeObject(object):
+    """ An Hour Of The Day """
+    def __init__(self, hour, precip, temp):
+        self.hour = int(hour)
+        self.precip = float(precip) # chance of precipitation
+        self.temp = float(temp)
+
+    def __str__(self):
+        rep = str(self.hour) + ":00\n" + str(self.precip) + "% chance rain\n" + str(self.temp) + " F\n"
+        return rep
+
+    def bike_logic(self):
+        global bike_to_work
+        if self.precip > lowest_acceptable_precip or self.temp < lowest_acceptable_temp:
+            bike_to_work = False
+
+def dark_sky_hourly_forecast():
+    """ Calls Dark Sky API Using 'darksky' Module And Returns Forecast Object """
+    key = dark_sky_api_key
+    exclude = "currently,minutely,daily,flags"
+    dark_sky_hourly_forecast = forecast(key, lat, lng, exclude = exclude)
+    return dark_sky_hourly_forecast
+
+def get_local_date_time(hour):
+    """ Accepts Unix Time Integer And Returns Date Time Object Converted To Local Time Zone """
+    utc = pytz.utc
+    utc_date_time = utc.localize(datetime.utcfromtimestamp(hour))
+    local_time_zone_object = timezone(local_time_zone)
+    local_date_time_object = utc_date_time.astimezone(local_time_zone_object)
+    return local_date_time_object
+
+def send_sns_sms(message):
+    """ Accepts String And Sends SMS To Phone Number """
     sns = boto3.client('sns')
-    print sns.publish(PhoneNumber = PHONE_NUMBER, Message = "Ride Your Bike!" + " " + "(" + time.strftime("%a") + ")")
+    print sns.publish(PhoneNumber = phone_number, Message = message)
     # print both executes and records status codes to CloudWatch
 
-def wunderground_hourly():
-    url = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/" + ZIP + ".json"
-    response = urllib2.urlopen(url) # Calling Wunderground
-    return json.load(response) # .json is read and converted to dictionary
-
 def lambda_handler(event, context):
-    weather = wunderground_hourly()
-    today = datetime.datetime.now().day # int representing today's day of the month
-    commuteTimes = [8, 9, 17, 18, 19] # hours during which I would conceivibly be commuting
-    hours = [] # Empty list into which I'll place (up to) five "hour" objects
+    global local_time_zone # Allows Change Made To "local_time_zone" To Be Viewable In All Other Functions
+    
+    # Dark Sky Hourly API Call
+    full_forecast = dark_sky_hourly_forecast()
 
-    for hour in weather['hourly_forecast']: # API returns 36 hour forecasts
-    	if int(hour['FCTTIME']['hour']) in commuteTimes and int(hour['FCTTIME']['mday']) == today:
-    		entry = HourObject(hour['FCTTIME']['hour'], hour['pop'], hour['temp']['english']) # Making object for each hour
-    		hours.append(entry) # Adding hour object to list
+    # Set local_time_zone String
+    local_time_zone = full_forecast.timezone
 
-    for hour in hours:
-    	hour.bike_logic() # Invoke each hour object's function to test whether global BIKE_TO_WORK should be toggled off
-    	print hour
+    # Slice Out Array Of Forecasts For Next 24 Hours
+    hourly_forecasts = full_forecast.hourly[:24]
 
-    if BIKE_TO_WORK == True:
-    	send_sns_sms()
-    	print time.strftime("%a %b %d") + "\nRide your bike!\nSMS Sent!"
+    # Make List Of Commute Forecast Objects
+    commute_times = [8, 9, 17, 18, 19] # Hours During Which I'd Conceivibly Be Biking
+    commute_forecasts = []
+
+    for hourly_forecast in hourly_forecasts:
+        local_date_time = get_local_date_time(hourly_forecast.time)
+
+        if local_date_time.hour in commute_times:
+            commute_forecasts.append(hourly_forecast)
+
+    # Make List of Bike Objects - Representing Each Potential Hour Of Commute With Bike Logic
+    bikes = []
+    for commute_forecast in commute_forecasts:
+        bike = BikeObject(get_local_date_time(commute_forecast.time).hour, commute_forecast.precipProbability, commute_forecast.temperature)
+        bikes.append(bike)
+
+    # Invoke Each Bike Object's 'bike_logic()' Function To Test Whether To Toggle 'bike_to_work' False
+    for bike in bikes:
+        bike.bike_logic()
+        print bike
+
+    if bike_to_work == True:
+        send_sns_sms("Ride Your Bike!" + " " + "(" + today_date_time.strftime("%A") + ")")
+        print today_date_time.strftime("%a %b %d %Y") + "\nRide your bike!\nSMS Sent!"
     else:
-    	print "It's too cold and/or rainy, bus it."
+        print "It's too cold and/or rainy, bus it."
